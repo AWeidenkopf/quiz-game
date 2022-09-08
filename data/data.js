c1713a62b81a5b3d24f8a77364965a50dee250b8
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-09-08 16:38:48 -0400

    implement initial css and data file

---
+++ data/data.js
@@ -0,0 +1,115 @@
+const category = [
+  movies = [
+    {
+    question:"Which famous singer appeared in the movie Mad Max: Beyond Thunderdome?",
+    answer: "Tina Turner",
+    options: ["Lady Gaga","Cyndi Lauper","Tina Turner","Ariana Grande"]
+  },
+  {
+    question:"Who directed the 1977 movie Star Wars?",
+    answer: "George Lucas",
+    options: ["George Lucas", "Steven Spielberg", "David Lynch", "Quentin Tarantino"]
+  },
+  {
+    question:"Which action movie star was the voice of the Iron Giant?",
+    answer: "Vin Diesel",
+    options: ["Vin Diesel", "Tom Cruise", "Johnny Depp", "Will Smith"]
+  },
+  {
+    question:"In what year was the movie “Grease” released?",
+    answer: "1978",
+    options: ["1980","1975","1970","1978"]
+  },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+  // {
+  //   question:,
+  //   answer: ,
+  //   options: 
+  // },
+
+],
+  animals = [],
+  sports = [],
+  food = []
+]
+
+console.log(category[0][0].options[0])
