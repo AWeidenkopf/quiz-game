2d2ad3e83aeb5556c540b14bd1b36355182a3fa8
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-09-09 17:51:46 -0400

    use relative paths for css/js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const categories = ["Movies", "Animals", "Food", "Computer"]
 
 /*-------------------------------- Variables --------------------------------*/
 
-let winner
+let winner, correctAnswer, score
 
 /*------------------------ Cached Element References ------------------------*/
 // Buttons
@@ -22,24 +22,30 @@ const categoryMsg = document.getElementById("category")
 const questionDisplay = document.getElementById("question-box")
 const arrOptions = Array.from(document.querySelectorAll(".option"))
 const scoreDisplay = document.getElementById("score")
+const playerAnwer = document.getElementById("quiz-container")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 resetBtn.addEventListener("click", init)
 moviesBtn.addEventListener("click", render)
+playerAnwer.addEventListener('click', handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
 
 init()
 
 function init() {
+  winner = null
+  score = 0
   categoryMsg.style.visibility = "hidden"
   resetBtn.style.visibility = "hidden"
+  scoreDisplay.textContent = `SCORE ${score}`
   scoreDisplay.style.visibility = "hidden"
   gameContainer.style.display = "none"
   categoryBox.style.display = "inline-block"
 }
+
 
 function render(e){
   categoryMsg.style.visibility = "visible"
@@ -47,7 +53,7 @@ function render(e){
   scoreDisplay.style.visibility = "visible"
   categoryBox.style.display = "none"
   gameContainer.style.display = "flex"
-  categoryMsg.textContent = `${e.target.textContent}`
+  categoryMsg.textContent = `${e.target.textContent.toUpperCase()}`
   renderQuestion()
 }
 
@@ -59,7 +65,30 @@ function renderQuestion(){
       for(let i = 0; i < arrOptions.length; i++){
         arrOptions[i].textContent = currQuestion.options[i]
       }
-      // arrOptions.forEach((option => currQuestion.options.forEach(el => option.textContent = el)))
+      return correctAnswer = currQuestion.answer
     }
   }
+  clearClass()
 }
+
+function handleClick(e){
+  const currAttribute = e.target.getAttribute('class')
+  if(e.target.textContent === correctAnswer){
+    e.target.setAttribute('class', currAttribute + ' correct')
+  } else {
+  e.target.setAttribute('class', currAttribute + ' wrong animate__animated animate__jello')
+  renderQuestion()
+  }
+
+}
+
+function clearClass(){
+for(let i = 0; i < arrOptions.length; i++){
+  const currClass = arrOptions[i].getAttribute('class')
+  console.log(currClass)
+  if(currClass.includes('wrong animate__animated animate__jello')){
+    currClass.replace('wrong animate__animated animate__jello', '')
+  }
+}
+console.log(currClass)
+  }
